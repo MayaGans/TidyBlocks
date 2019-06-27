e077fcf8d02d89c1cfd4f8485f07a7fa071477d3
Maya <jaffe.maya@gmail.com>
2019-06-27 09:15:22 -0600

    remove summary function in pivot script

--- dummy_dataforge_examples/pivotFunction.js
+++ dummy_dataforge_examples/pivotFunction.js
@@ -31,18 +31,6 @@ const dataForge = require('data-forge')
 // Group by client.
 
 
-const summarized = sampleDF_1
-    .groupBy(row => row.Col2)
-    .select(group => ({
-        ClientName: group.first().Col2,
-        Amount: group.deflate(row => row.Col1).sum(), // Sum sales per client.
-    }))
-    .inflate() // Series -> dataframe.
-    .toArray(); // Convert to regular JS array.
-
-console.log(summarized)
-
-
 const columnsToGroupBy = ["Col2", "Col3"];
 const pivotted = sampleDF_1
 	.pivot(columnsToGroupBy, {
@@ -50,4 +38,6 @@ const pivotted = sampleDF_1
         OutputColumn1: series => series.sum(),
         OutputColumn2: series => series.average(),
     }
-}).inflate().toArray()
+})
+
+console.log(pivotted)
