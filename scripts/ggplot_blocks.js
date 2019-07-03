4721372b545772bf6e21978e78a9d320796591d4
Maya <jaffe.maya@gmail.com>
2019-07-03 14:16:08 -0600

    minor changes to make plots work - they still dont

--- scripts/ggplot_blocks.js
+++ scripts/ggplot_blocks.js
@@ -2,7 +2,7 @@
 
 {
   "type": "ggplot_bar",
-  "message0": "PLOT %1 %2 %3 %4 %5",
+  "message0": "BAR %1 %2 %3 %4 %5",
   "args0": [
     {
       "type": "input_dummy"
@@ -31,7 +31,8 @@
   "style": "ggplot_blocks",
   "tooltip": "",
   "helpUrl": ""
-},{
+},
+{
   "type": "ggplot_histogram",
   "message0": "HISTOGRAM %1 %2",
   "args0": [
@@ -50,6 +51,54 @@
   "style": "ggplot_blocks",
   "tooltip": "",
   "helpUrl": ""
+},
+{
+  "type": "ggplot_box",
+  "message0": "BOX %1 %2 %3 %4 %5",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "field_input",
+      "name": "X",
+      "text": "X"
+    },
+    {
+      "type": "input_value",
+      "name": "X"
+    },
+    {
+      "type": "field_input",
+      "name": "Y",
+      "text": "Y"
+    },
+    {
+      "type": "input_value",
+      "name": "Y"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "style": "ggplot_blocks",
+  "tooltip": "",
+  "helpUrl": ""
 }
 
 ]);
+
+Blockly.Blocks['ggplot_histogram2'] = {
+  init: function() {
+    this.appendValueInput("Columns")
+        .setCheck(null)
+        .appendField("HISTOGRAM");
+    this.appendDummyInput()
+        .appendField("bins:")
+        .appendField(new Blockly.FieldTextInput("bins"), "bins");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setStyle("ggplot_blocks")
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
