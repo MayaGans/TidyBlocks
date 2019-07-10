4a08dc0ba630f1d06a05dc54f689118ec9dca634
Greg Wilson <gvwilson@third-bit.com>
2019-07-10 18:18:44 -0400

    Removing Node modules from Git repository.
    1.  Developers usually don't add third-party libraries to their repository, but rely on people to install them use `npm install` (which in turn looks in `package.json`).
    2.  The `.gitignore` file added in this commit ignores `node_modules` in future, and also a lot of other stuff - it comes from https://github.com/github/gitignore/blob/master/Node.gitignore.

--- node_modules/capture-template/build/cli.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-(new index_1.ExampleClass()).test();
-//TODO: Code for your command line app goes here.
-//# sourceMappingURL=cli.js.map
