28a60c25cc57878dfb902055e6a714f83c11e589
Maya <jaffe.maya@gmail.com>
2019-08-01 15:00:49 -0600

    add parseFloat to extract regression line components in ggplot_point

--- scripts/ggplot_generator.js
+++ scripts/ggplot_generator.js
@@ -115,57 +115,14 @@ Blockly.JavaScript['ggplot_bar'] = function(block) {
       return obj; //Continue to the next object in the array
   });
   
-  function findLineByLeastSquares(values_x, values_y) {
-      var x_sum = 0;
-      var y_sum = 0;
-      var xy_sum = 0;
-      var xx_sum = 0;
-      var count = 0;
+  console.log(result.data)
   
-      /*
-       * The above is just for quick access, makes the program faster
-       */
-      var x = 0;
-      var y = 0;
-      var values_length = values_x.length;
+  var lineDat = findLineByLeastSquares(result.${argument0}.map(parseFloat), 
+  									   result.${argument1}.map(parseFloat))
   
-      if (values_length != values_y.length) {
-          throw new Error('The parameters values_x and values_y need to have same size!');
-      }
+  console.log(lineDat[0])
+  console.log(lineDat[1])
   
-      /*
-       * Above and below cover edge cases
-       */
-      if (values_length === 0) {
-          return [ [], [] ];
-      }
-  
-      /*
-       * Calculate the sum for each of the parts necessary.
-       */
-      for (let i = 0; i< values_length; i++) {
-          x = values_x[i];
-          y = values_y[i];
-          x_sum+= x;
-          y_sum+= y;
-          xx_sum += x*x;
-          xy_sum += x*y;
-          count++;
-      }
-  
-      /*
-       * Calculate m and b for the line equation:
-       * y = x * m + b
-       */
-      var m = (count*xy_sum - x_sum*y_sum) / (count*xx_sum - x_sum*x_sum);
-      var b = (y_sum/count) - (m*x_sum)/count;
-  
-      // solve for x and y intercept
-      return [m, b]
-  }
-  
-  const lineDat = findLineByLeastSquares(result.${argument0}, result.${argument1})
-
     let spec = {
       "width": 700,
       "layer": [
